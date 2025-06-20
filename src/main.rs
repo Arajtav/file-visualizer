7d14c0ff17b133e54867d464b0b24dba1688aa7b
Aräjtav <arajtav@arajtav.com>
2025-06-20 17:52:13 +0200

    feat: create cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+use clap::ValueEnum;
+
+#[derive(Debug, Clone, ValueEnum)]
+#[clap(rename_all = "lower")]
+enum Mode {}
+
+#[derive(Parser, Debug)]
+#[command(long_about = None)]
+struct Args {
+    /// Mode
+    #[arg(short, long)]
+    mode: Mode,
+
+    /// Input file
+    file: PathBuf,
+
+    /// Output file
+    out: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
